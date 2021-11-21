6b82d760cc5deff3b9722f2eec55c8d10847d3dd
fedyksr <60010253+fedyksr@users.noreply.github.com>
2021-11-21 22:39:40 +0000

    completed my website

--- dentist/dentist/settings.py
+++ dentist/dentist/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT='587'
+EMAIL_PORT= 587
 EMAIL_HOST_USER='fedyksr@gmail.com '
 EMAIL_HOST_PASSWORD='qdnplvvwkrqnqdwo'
 EMAIL_USE_TLS=True
--- dentist/website/urls.py
+++ dentist/website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('index.html',views.index ,name='index'),
     path('',views.index ,name='index'),
     path('contact.html',views.contact,name='contact'),
+    path('about.html',views.about ,name='about'),
+    path('pricing.html',views.pricing ,name='pricing'),
+    path('service.html',views.service,name='service'),
 ]
