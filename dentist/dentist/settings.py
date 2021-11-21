756b55f272a8ace84fa69ba8fd9930bbed6bf825
fedyksr <60010253+fedyksr@users.noreply.github.com>
2021-11-21 14:43:44 +0000

    setting mail

--- dentist/dentist/settings.py
+++ dentist/dentist/settings.py
@@ -128,3 +128,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST='127.0.0.1'
+EMAIL_PORT='1025'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=False
+#EMAIL_USE_SSL=False
