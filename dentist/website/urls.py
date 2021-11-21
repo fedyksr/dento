ce2d7cb8b2ab6b7e468b7194b7ce8d8e7b08c309
fedyksr <60010253+fedyksr@users.noreply.github.com>
2021-11-21 14:28:05 +0000

    add contact.html and send mail

--- dentist/website/urls.py
+++ dentist/website/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index ,name='index'),
+    path('contact.html',views.contact,name='contact'),
 ]
--- dentist/website/views.py
+++ dentist/website/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def index (request):
     return render(request, 'index.html',{})
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        #send_mail
+        send_mail(
+            message_name , #subject
+            message , #message
+            message_email, #from email
+            ['fedyksr@gmail.com'] , #To email
+        )
+        
+        return render(request, 'contact.html',{'message_name':message_name})
+    else:
+        return render(request, 'contact.html',{})
